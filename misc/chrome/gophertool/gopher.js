fbb0fe4da09b1983ca0942df55069658f72c556e
Sourcegraph <batch-changes@sourcegraph.com>
2021-07-28 22:56:08 +0000

    update-to-arrow-functions

--- misc/chrome/gophertool/gopher.js
+++ misc/chrome/gophertool/gopher.js
@@ -7,7 +7,7 @@ var commitRE = /^(?:\d+:)?([0-9a-f]{6,40})$/; // e.g "8486:ab29d2698a47" or "ab2
 var gerritChangeIdRE = /^I[0-9a-f]{4,40}$/; // e.g. Id69c00d908d18151486007ec03da5495b34b05f5
 var pkgRE = /^[a-z0-9_\/]+$/;
 
-function urlForInput(t) {
+const :[fn~\w+] = (t) => {
     if (!t) {
         return null;
     }
